324ed3f4580fb54f0f9974bf69cbd66ecc992bc9
Johan Magnusson <github@johanmagnusson.se>
2016-05-07 00:49:19 +0200

    New: Add first draft for fragment views. New: Use HelloCharts on summary view Change: SectionPageAdapter return new fragments.

--- app/src/main/java/se/johanmagnusson/android/adomile/MainActivity.java
+++ app/src/main/java/se/johanmagnusson/android/adomile/MainActivity.java
@@ -1,22 +1,18 @@
 package se.johanmagnusson.android.adomile;
 
+import android.os.Bundle;
 import android.support.design.widget.TabLayout;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -53,16 +49,7 @@ public class MainActivity extends AppCompatActivity {
 
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(mViewPager);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
+        tabLayout.getTabAt(1).select();
     }
 
 
@@ -137,7 +124,14 @@ public class MainActivity extends AppCompatActivity {
         public Fragment getItem(int position) {
             // getItem is called to instantiate the fragment for the given page.
             // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
+            switch (position) {
+                case 0:
+                    return new SummaryFragment();
+                case 1:
+                    return new RegisterFragment();
+                default:
+                    return new LogFragment();
+            }
         }
 
         @Override
@@ -150,11 +144,11 @@ public class MainActivity extends AppCompatActivity {
         public CharSequence getPageTitle(int position) {
             switch (position) {
                 case 0:
-                    return "SECTION 1";
+                    return getString(R.string.tab_summary);
                 case 1:
-                    return "SECTION 2";
+                    return getString(R.string.tab_register);
                 case 2:
-                    return "SECTION 3";
+                    return getString(R.string.tab_log);
             }
             return null;
         }
