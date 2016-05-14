6a590f27d75236f59bb57eaa7024df634d588d5c
Johan Magnusson <github@johanmagnusson.se>
2016-05-14 11:31:34 +0200

    New: Material icon opacity. New: Add trip type to TripColums. New: Save date, destination, mileage as private or work. New: Save date, destination, mileage as private or work on rotation.

--- app/src/main/java/se/johanmagnusson/android/adomile/RegisterFragment.java
+++ app/src/main/java/se/johanmagnusson/android/adomile/RegisterFragment.java
@@ -7,13 +7,12 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
@@ -31,11 +30,15 @@ public class RegisterFragment extends Fragment{
     private final String KEY_DESTINATION = "destination";
     private final String KEY_MILEAGE = "mileage";
     private final String KEY_NOTE = "note";
+    private final String KEY_TRIP_TYPE = "trip_type";
     private final String KEY_PREVIOUS_TRIP = "previous_trip";
 
+    private final Integer PRIVATE = 0;
+    private final Integer WORK = 1;
+
     private TextView mDate;
-    private TextInputLayout mDestination;
-    private TextInputLayout mMileage;
+    private EditText mDestination;
+    private EditText mMileage;
     private Button mPrivate;
     private Button mWork;
     private ViewGroup mPreviousTripContainer;
@@ -64,8 +67,8 @@ public class RegisterFragment extends Fragment{
             }
         });
 
-        mDestination = (TextInputLayout) root.findViewById(R.id.destination_input_layout);
-        mMileage = (TextInputLayout) root.findViewById(R.id.mileage_input_layout);
+        mDestination = (EditText) root.findViewById(R.id.destination);
+        mMileage = (EditText) root.findViewById(R.id.mileage);
 
         //todo: handle add note
 
@@ -103,16 +106,18 @@ public class RegisterFragment extends Fragment{
 
             // Destination
             if(savedInstanceState.containsKey(KEY_DESTINATION))
-                mDestination.getEditText().setText(savedInstanceState.getString(KEY_DESTINATION));
+                mDestination.setText(savedInstanceState.getString(KEY_DESTINATION));
 
             // Mileage
             if(savedInstanceState.containsKey(KEY_MILEAGE))
-                mMileage.getEditText().setText(savedInstanceState.getString(KEY_MILEAGE));
+                mMileage.setText(savedInstanceState.getString(KEY_MILEAGE));
 
             // Note
             //todo: add note
 //            if(savedInstanceState.containsKey(KEY_NOTE))
 //                mNote.getEditText().setText(savedInstanceState.getString(KEY_NOTE));
+
+            //todo: work or private
 
             // Previous trip
             if(savedInstanceState.containsKey(KEY_PREVIOUS_TRIP)) {
@@ -130,11 +135,11 @@ public class RegisterFragment extends Fragment{
     private void registerTrip(boolean isWork) {
         ContentValues cv = new ContentValues();
         cv.put(TripColumns.Date, mDate.getText().toString());
-        Log.d(TAG, "---------- saving dte: " + mDate.getText().toString());
-        cv.put(TripColumns.Destination, mDestination.getEditText().getText().toString());
-        cv.put(TripColumns.Mileage, Integer.parseInt(mMileage.getEditText().getText().toString()));
+        cv.put(TripColumns.Destination, mDestination.getText().toString());
+        cv.put(TripColumns.Mileage, Integer.parseInt(mMileage.getText().toString()));
         //todo: add note
         cv.put(TripColumns.Note, "Testing...");
+        cv.put(KEY_TRIP_TYPE, isWork ? WORK : PRIVATE);
 
         //todo: async?
         Uri uri = getActivity().getContentResolver().insert(TripProvider.Trips.CONTENT_URI, cv);
@@ -146,25 +151,22 @@ public class RegisterFragment extends Fragment{
     }
 
     private void showPreviousTripCard(Uri previousTrip) {
-        Log.d(TAG, "--------- Trip Card");
 
         // Is the card added to the view?
         if(mPreviousTripContainer.getChildCount() < 1) {
             mPreviousTripContainer.addView(mTripCard);
         }
 
-        Cursor cursor = getActivity().getContentResolver().query(previousTrip,null, null, null, null);
+        Cursor cursor = getActivity().getContentResolver().query(previousTrip, null, null, null, null);
 
         if(cursor.moveToFirst()) {
+            //todo: format date to "today" if current date
             mTripCardDate.setText(CursorHelper.getString(cursor, TripColumns.Date));
-            Log.d(TAG, "---------- loading dte: " + CursorHelper.getString(cursor, TripColumns.Date));
             mTripCardDestination.setText(CursorHelper.getString(cursor, TripColumns.Destination));
             mTripCardMileage.setText(CursorHelper.getString(cursor, TripColumns.Mileage));
             //todo: add note
             //todo: set icon, work or private
         }
-        else
-            Log.d(TAG, "--------- Cursor empty");
 
         //todo: calculate km with the trip before this one. show dash (-) if no previous trip
     }
@@ -176,14 +178,16 @@ public class RegisterFragment extends Fragment{
             outState.putString(KEY_DATE, mDate.getText().toString());
 
         if(mDestination != null)
-            outState.putString(KEY_DESTINATION, mDestination.getEditText().getText().toString());
+            outState.putString(KEY_DESTINATION, mDestination.getText().toString());
 
         if(mMileage != null)
-            outState.putString(KEY_MILEAGE, mMileage.getEditText().getText().toString());
+            outState.putString(KEY_MILEAGE, mMileage.getText().toString());
 
         //todo: add note
 //        if(mNote != null)
 //            outState.putString(KEY_NOTE, mNote.getEditText().getText().toString());
+
+        //todo: work or private
 
         if(mPreviousTrip != null)
             outState.putString(KEY_PREVIOUS_TRIP, mPreviousTrip.toString());
