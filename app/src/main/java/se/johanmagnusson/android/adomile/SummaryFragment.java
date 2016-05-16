8be8c28bc9b63aa9b99ec4833132ab47144c5e14
Johan Magnusson <github@johanmagnusson.se>
2016-05-16 22:37:18 +0200

    New: Show chart with dummy data.

--- app/src/main/java/se/johanmagnusson/android/adomile/SummaryFragment.java
+++ app/src/main/java/se/johanmagnusson/android/adomile/SummaryFragment.java
@@ -7,16 +7,62 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import lecho.lib.hellocharts.model.PieChartData;
+import lecho.lib.hellocharts.model.SliceValue;
+import lecho.lib.hellocharts.util.ChartUtils;
+import lecho.lib.hellocharts.view.PieChartView;
 
 public class SummaryFragment extends Fragment{
+
+    private PieChartView mChart;
+    private PieChartData mChartData;
+
+    private TextView mTotalPrivate;
+    private TextView mTotalWork;
+    private TextView mPeriod;
+    private TextView mInbound;
+    private TextView mOutbound;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        //return super.onCreateView(inflater, container, savedInstanceState);
-
         View root = inflater.inflate(R.layout.fragment_summary, container, false);
 
+        mChart = (PieChartView) root.findViewById(R.id.chart);
+        mTotalPrivate = (TextView) root.findViewById(R.id.total_private_mileage);
+        mTotalWork = (TextView) root.findViewById(R.id.total_work_mileage);
+        mPeriod = (TextView) root.findViewById(R.id.period);
+        mInbound = (TextView) root.findViewById(R.id.inbound_km);
+        mOutbound = (TextView) root.findViewById(R.id.outbound_km);
+
+        mChart.setChartRotationEnabled(false);
+        mChart.setClickable(false);
+
+        updateChart();
+
         return root;
+    }
+
+    //todo: get data from db, use asyncTask to fetch and calculate
+    //todo: handle if no data for the period, show some icon instead of chart?
+    private void updateChart() {
+        int numValues = 2;
+
+        List<SliceValue> values = new ArrayList<SliceValue>();
+
+        for (int i = 0; i < numValues; ++i) {
+            SliceValue sliceValue = new SliceValue((float) Math.random() * 30 + 15, ChartUtils.pickColor());
+            values.add(sliceValue);
+        }
+
+        mChartData = new PieChartData(values);
+        mChartData.setHasCenterCircle(true);
+
+        mChart.setPieChartData(mChartData);
     }
 }
